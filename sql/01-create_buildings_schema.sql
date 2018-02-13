1710a9d37da776c48f4a71e0d98c80bf76fc15d6
Daniel Silk <dsilk@linz.govt.nz>
2018-02-13 15:31:32 +1300

    Add building table to hold persistant ID

--- sql/01-create_buildings_schema.sql
+++ sql/01-create_buildings_schema.sql
@@ -41,10 +41,21 @@ DROP INDEX IF EXISTS idx_capture_source_capture_source_group_id;
 CREATE INDEX idx_capture_source_capture_source_group_id
     ON buildings.capture_source USING btree (capture_source_group_id);
 
+-- Building
+
+CREATE TABLE IF NOT EXISTS buildings.buildings (
+      building_id serial PRIMARY KEY
+    , begin_lifespan timestamptz
+    , end_lifespan timestamptz
+);
+
+SELECT setval('buildings.buildings_building_id_seq', 1000000);
+
 -- Building Outlines
 
 CREATE TABLE IF NOT EXISTS buildings.building_outlines (
       building_outline_id serial PRIMARY KEY
+    , building_id integer NOT NULL REFERENCES buildings.buildings (building_id)
     , capture_method_id integer NOT NULL REFERENCES buildings.capture_method (capture_method_id)
     , capture_source_id integer NOT NULL REFERENCES buildings.capture_source (capture_source_id)
     , lifecycle_stage_id integer NOT NULL REFERENCES buildings.lifecycle_stage (lifecycle_stage_id)
@@ -54,6 +65,10 @@ CREATE TABLE IF NOT EXISTS buildings.building_outlines (
 );
 
 SELECT setval('buildings.building_outlines_building_outline_id_seq', 1000000);
+
+DROP INDEX IF EXISTS idx_building_outlines_building_id;
+CREATE INDEX idx_building_outlines_building_id
+    ON buildings.building_outlines USING btree (building_id);
 
 DROP INDEX IF EXISTS idx_building_outlines_capture_method_id;
 CREATE INDEX idx_building_outlines_capture_method_id
