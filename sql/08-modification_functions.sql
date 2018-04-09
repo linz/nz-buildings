8fe1bb405dcbbe94667166a17bf592d67f6bcaee
Megan <MDavidson@linz.govt.nz>
2018-04-09 17:02:09 +1200

    more functions

--- sql/08-modification_functions.sql
+++ sql/08-modification_functions.sql
@@ -23,7 +23,7 @@ $$
     LIMIT    1;
 
 $$
-LANGUAGE sql STABLE;
+LANGUAGE sql VOLATILE;
 
 -------------------------------------------------------------------------
 -- SUBURB INTERSECTION- Replace the suburb values with the intersection result
@@ -53,7 +53,7 @@ BEGIN
 END;
 
 $$
-LANGUAGE plpgsql;
+LANGUAGE plpgsql VOLATILE;
 
 --------------------------------------------------------------------------
 -- TOWN/CITY INTERSECTION- find the id of the town/city that has the most overlap with
@@ -62,7 +62,7 @@ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION buildings.fn_town_city_locality_intersect_poly(
     p_polygon_geometry geometry
 )
-RETURNS integer AS
+RETURNS numeric(10,0) AS
 $$
     
     SELECT   nzl.city_id
@@ -80,7 +80,7 @@ $$
     LIMIT    1;
 
 $$
-LANGUAGE sql STABLE;
+LANGUAGE sql VOLATILE;
 
 -------------------------------------------------------------------------
 -- TOWN/CITY INTERSECTION- Replace the town/city values with the intersection result
@@ -110,7 +110,7 @@ BEGIN
 END;
 
 $$
-LANGUAGE plpgsql;
+LANGUAGE plpgsql VOLATILE;
 
 --------------------------------------------------------------------------
 -- TERRITORIAL AUTHORITY INTERSECTION- find the id of the TA that has the most overlap with
@@ -137,7 +137,7 @@ $$
     LIMIT    1;
 
 $$
-LANGUAGE sql STABLE;
+LANGUAGE sql VOLATILE;
 
 -------------------------------------------------------------------------
 -- TERRITORIAL AUTHORITY INTERSECTION- Replace the TA values with the intersection result
@@ -167,7 +167,7 @@ BEGIN
 END;
 
 $$
-LANGUAGE plpgsql;
+LANGUAGE plpgsql VOLATILE;
 
 -------------------------------------------------------------------
 --BUILDINGS insert into
@@ -187,7 +187,7 @@ $$
     RETURNING building_id;
 
 $$
-LANGUAGE sql STABLE;
+LANGUAGE sql VOLATILE;
 
 ----------------------------------------------------------------
 -- BUILDING OUTLINES insert into
@@ -233,12 +233,12 @@ $$
     RETURNING building_outline_id;
 
 $$
-LANGUAGE sql STABLE;
+LANGUAGE sql VOLATILE;
 
 -------------------------------------------------------------------------
 -- BULK LOAD OUTLINES insert into
 -------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION buildings_bulk_load.bulk_load_outlines_insert(
+CREATE OR REPLACE FUNCTION buildings_bulk_load.fn_bulk_load_outlines_insert(
       p_supplied_dataset_id integer
     , p_external_outline_id integer
     , p_bulk_load_status_id integer
@@ -247,7 +247,6 @@ CREATE OR REPLACE FUNCTION buildings_bulk_load.bulk_load_outlines_insert(
     , p_suburb_locality_id integer
     , p_town_city_id integer
     , p_territorial_authority_id integer
-    , p_begin_lifespan timestamptz
     , p_shape geometry
 )
 RETURNS integer AS
@@ -276,10 +275,196 @@ $$
         , NULL --p_suburb_locality_id
         , NULL --p_town_city_id
         , NULL --p_territorial_authority_id
-        , NULL --p_begin_lifespan
+        , now() --p_begin_lifespan
         , p_shape
     )
     RETURNING bulk_load_outline_id;
 
 $$
-LANGUAGE sql STABLE;
+LANGUAGE sql VOLATILE;
+
+-------------------------------------------------------------------------
+-- EXISTING SUBSET EXTRACT insert into
+  -- returns number of rows inserted into table
+-------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION buildings_bulk_load.fn_existing_subset_extract_insert(
+      p_building_outline_id integer
+    , p_supplied_dataset_id integer
+    , p_shape geometry
+)
+RETURNS integer AS
+$$
+
+DECLARE
+    v_rows_updated integer;
+
+BEGIN
+
+    INSERT INTO buildings_bulk_load.existing_subset_extract(
+          building_outline_id
+        , supplied_dataset_id
+        , shape
+    )
+    VALUES (
+          p_building_outline_id
+        , p_supplied_dataset_id
+        , p_shape
+    );
+
+    GET DIAGNOSTICS v_rows_updated = ROW_COUNT;
+
+    RETURN v_rows_updated;
+END;
+
+$$
+LANGUAGE plpgsql VOLATILE;
+
+-------------------------------------------------------------------------
+-- SUPPLIED DATASET insert into
+-------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION buildings_bulk_load.fn_supplied_datasets_insert(
+      p_description varchar(250)
+    , p_supplier_id integer
+)
+RETURNS integer AS
+$$
+
+    INSERT INTO buildings_bulk_load.supplied_datasets(
+          supplied_dataset_id
+        , description
+        , supplier_id
+        , processed_date
+        , transfer_date
+    )
+    VALUES (
+          DEFAULT -- sequence
+        , p_description
+        , p_supplier_id
+        , NULL --processed_date
+        , NULL --transfer_date
+    )
+    RETURNING supplied_dataset_id;
+
+$$
+LANGUAGE sql VOLATILE;
+
+-------------------------------------------------------------------------
+-- CAPTURE SOURCE insert into
+-------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION buildings_common.fn_capture_source_insert(
+      p_capture_source_group_id integer
+    , p_external_source_id varchar(250)
+)
+RETURNS integer AS
+$$
+
+    INSERT INTO buildings_common.capture_source(
+          capture_source_id
+        , capture_source_group_id
+        , external_source_id
+    )
+    VALUES (
+          DEFAULT -- sequence
+        , p_capture_source_group_id
+        , p_external_source_id
+
+    )
+    RETURNING capture_source_id;
+
+$$
+LANGUAGE sql VOLATILE;
+
+-------------------------------------------------------------------------
+-- ORGANISATION insert into
+-------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION buildings_bulk_load.fn_organisation_insert(
+      p_value varchar(250)
+)
+RETURNS integer AS
+$$
+
+    INSERT INTO buildings_bulk_load.organisation(
+          organisation_id
+        , value
+    )
+    VALUES (
+          DEFAULT -- sequence
+        , p_value
+
+    )
+    RETURNING organisation_id;
+
+$$
+LANGUAGE sql VOLATILE;
+
+-------------------------------------------------------------------------
+-- CAPTURE METHOD insert into
+-------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION buildings_common.fn_capture_method_insert(
+      p_value varchar(250)
+)
+RETURNS integer AS
+$$
+
+    INSERT INTO buildings_common.capture_method(
+          capture_method_id
+        , value
+    )
+    VALUES (
+          DEFAULT -- sequence
+        , p_value
+
+    )
+    RETURNING capture_method_id;
+
+$$
+LANGUAGE sql VOLATILE;
+
+-------------------------------------------------------------------------
+-- LIFECYCLE STAGE insert into
+-------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION buildings.fn_lifecycle_stage_insert(
+      p_value varchar(40)
+)
+RETURNS integer AS
+$$
+
+    INSERT INTO buildings.lifecycle_stage(
+          lifecycle_stage_id
+        , value
+    )
+    VALUES (
+          DEFAULT -- sequence
+        , p_value
+
+    )
+    RETURNING lifecycle_stage_id;
+
+$$
+LANGUAGE sql VOLATILE;
+
+-------------------------------------------------------------------------
+-- CAPTURE SOURCE GROUP insert into
+-------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION buildings_common.fn_capture_source_group_insert(
+      p_value varchar(80)
+    , p_description varchar(400)
+)
+RETURNS integer AS
+$$
+
+    INSERT INTO buildings_common.capture_source_group(
+          capture_source_group_id
+        , value
+        , description
+    )
+    VALUES (
+          DEFAULT -- sequence
+        , p_value
+        , p_description
+
+    )
+    RETURNING capture_source_group_id;
+
+$$
+LANGUAGE sql VOLATILE;
