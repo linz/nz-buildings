69867ca5eff8196b3b6fb087ff54f94124232a23
Unknown <MDavidson@linz.govt.nz>
2018-05-11 11:22:41 +1200

    Tidy Up

--- tests/functiontestdata/03-insert_test_data_functions.sql
+++ tests/functiontestdata/03-insert_test_data_functions.sql
@@ -9,7 +9,7 @@ SELECT buildings_common.capture_method_insert('Test Capture Method');
 -- lifecycle stage
 SELECT buildings.lifecycle_stage_insert('Test Lifecycle Stage');
 -- supplied datasets
-SELECT buildings_bulk_load.supplied_datasets_insert('test_dataset', 1);
+SELECT buildings_bulk_load.supplied_datasets_insert('Test Dataset', 1);
 -- bulk load outline
 SELECT buildings_bulk_load.bulk_load_outlines_insert(1, NULL, 1, 5, 1, 1, NULL, 1, '0106000020910800000100000001030000000100000005000000000000009FB33A4100000080BCB35441000000009FB33A4100000080AEB3544100000000D8B33A4100000080AEB3544100000000D8B33A4100000080BCB35441000000009FB33A4100000080BCB35441');
 -- building
