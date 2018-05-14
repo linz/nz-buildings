0a5f076ae0e5cc7cefaa43e21ce87ffabaacd9af
Yingting <YChen@linz.govt.nz>
2018-05-14 15:38:50 +1200

    auto update view for outlines in different relationships

--- buildings/gui/alter_building_relationships.py
+++ buildings/gui/alter_building_relationships.py
@@ -59,6 +59,7 @@ class AlterRelationships(QFrame, FORM_CLASS):
         self.init_list(self.lst_existing)
         self.init_list(self.lst_bulk)
 
+        self.update_view()
         self.add_building_lyrs()
 
         iface.setActiveLayer(self.lyr_existing)
@@ -78,6 +79,41 @@ class AlterRelationships(QFrame, FORM_CLASS):
 
         self.lst_existing.itemSelectionChanged.connect(self.select_from_lst_existing)
         self.lst_bulk.itemSelectionChanged.connect(self.select_from_lst_bulk)
+
+    def update_view(self):
+        """Update view for buildings in different relationships"""
+
+        sql_refresh = '''CREATE OR REPLACE VIEW buildings_bulk_load.added_outlines AS
+                    SELECT a.bulk_load_outline_id, b.shape
+                    FROM buildings_bulk_load.added a
+                    JOIN buildings_bulk_load.bulk_load_outlines b USING (bulk_load_outline_id);
+
+                CREATE OR REPLACE VIEW buildings_bulk_load.removed_outlines AS
+                    SELECT r.building_outline_id, e.shape
+                    FROM buildings_bulk_load.removed r
+                    JOIN buildings_bulk_load.existing_subset_extracts e USING (building_outline_id);
+
+                CREATE OR REPLACE VIEW buildings_bulk_load.matched_bulk_load_outlines AS
+                    SELECT m.bulk_load_outline_id, b.shape
+                    FROM buildings_bulk_load.matched m
+                    JOIN buildings_bulk_load.bulk_load_outlines b USING (bulk_load_outline_id);
+
+                CREATE OR REPLACE VIEW buildings_bulk_load.related_bulk_load_outlines AS
+                    SELECT DISTINCT r.bulk_load_outline_id, b.shape
+                    FROM buildings_bulk_load.related r
+                    JOIN buildings_bulk_load.bulk_load_outlines b USING (bulk_load_outline_id);
+
+                CREATE OR REPLACE VIEW buildings_bulk_load.matched_existing_outlines AS
+                    SELECT m.building_outline_id, e.shape
+                    FROM buildings_bulk_load.matched m
+                    JOIN buildings_bulk_load.existing_subset_extracts e USING (building_outline_id);
+
+                CREATE OR REPLACE VIEW buildings_bulk_load.related_existing_outlines AS
+                    SELECT DISTINCT r.building_outline_id, e.shape
+                    FROM buildings_bulk_load.related r
+                    JOIN buildings_bulk_load.existing_subset_extracts e USING (building_outline_id);
+                '''
+        self.db._execute(sql_refresh)
 
     def add_building_lyrs(self):
         """ Add building layers """
@@ -972,37 +1008,6 @@ class AlterRelationships(QFrame, FORM_CLASS):
                             DISCARD TEMP;
                             '''
 
-        sql_refresh = '''CREATE OR REPLACE VIEW buildings_bulk_load.added_outlines AS
-                            SELECT a.bulk_load_outline_id, b.shape
-                            FROM buildings_bulk_load.added a
-                            JOIN buildings_bulk_load.bulk_load_outlines b USING (bulk_load_outline_id);
-
-                        CREATE OR REPLACE VIEW buildings_bulk_load.removed_outlines AS
-                            SELECT r.building_outline_id, e.shape
-                            FROM buildings_bulk_load.removed r
-                            JOIN buildings_bulk_load.existing_subset_extracts e USING (building_outline_id);
-
-                        CREATE OR REPLACE VIEW buildings_bulk_load.matched_bulk_load_outlines AS
-                            SELECT m.bulk_load_outline_id, b.shape
-                            FROM buildings_bulk_load.matched m
-                            JOIN buildings_bulk_load.bulk_load_outlines b USING (bulk_load_outline_id);
-
-                        CREATE OR REPLACE VIEW buildings_bulk_load.related_bulk_load_outlines AS
-                            SELECT DISTINCT r.bulk_load_outline_id, b.shape
-                            FROM buildings_bulk_load.related r
-                            JOIN buildings_bulk_load.bulk_load_outlines b USING (bulk_load_outline_id);
-
-                        CREATE OR REPLACE VIEW buildings_bulk_load.matched_existing_outlines AS
-                            SELECT m.building_outline_id, e.shape
-                            FROM buildings_bulk_load.matched m
-                            JOIN buildings_bulk_load.existing_subset_extracts e USING (building_outline_id);
-
-                        CREATE OR REPLACE VIEW buildings_bulk_load.related_existing_outlines AS
-                            SELECT DISTINCT r.building_outline_id, e.shape
-                            FROM buildings_bulk_load.related r
-                            JOIN buildings_bulk_load.existing_subset_extracts e USING (building_outline_id);
-                        '''
-
         self.db.open_cursor()
 
         for row in range(self.lst_existing.count())[::-1]:
@@ -1047,11 +1052,11 @@ class AlterRelationships(QFrame, FORM_CLASS):
                 self.db.execute_no_commit(sql_new_related_prep, (id_bulk, id_existing))
         self.db.execute_no_commit(sql_new_related)
 
-        # refresh view layer
-        self.db.execute_no_commit(sql_refresh)
-
         if commit_status:
             self.db.commit_open_cursor()
+
+        # refresh view layer
+        self.update_view()
 
         self.lst_existing.clear()
         self.lst_bulk.clear()
