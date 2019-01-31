18571a62498bbdadc95339cc79b1234ed2807ff8
Daniel Silk <dsilk@linz.govt.nz>
2019-01-31 15:22:12 +1300

    Add deploy/revert/verify files for default values

---
+++ db/sql/deploy/buildings_bulk_load/default_values.sql
@@ -0,0 +1,20 @@
+-- Deploy buildings:buildings_bulk_load/default_values to pg
+
+BEGIN;
+
+-- organisation
+INSERT INTO buildings_bulk_load.organisation (value) VALUES ('Ecopia');
+
+-- bulk_load_status
+INSERT INTO buildings_bulk_load.bulk_load_status (value) VALUES ('Supplied');
+INSERT INTO buildings_bulk_load.bulk_load_status (value) VALUES ('Added During QA');
+INSERT INTO buildings_bulk_load.bulk_load_status (value) VALUES ('Deleted During QA');
+
+-- qa_status
+INSERT INTO buildings_bulk_load.qa_status (value) VALUES ('Not Checked');
+INSERT INTO buildings_bulk_load.qa_status (value) VALUES ('Okay');
+INSERT INTO buildings_bulk_load.qa_status (value) VALUES ('Pending');
+INSERT INTO buildings_bulk_load.qa_status (value) VALUES ('Refer to Supplier');
+INSERT INTO buildings_bulk_load.qa_status (value) VALUES ('Not Removed');
+
+COMMIT;
---
+++ db/sql/verify/buildings/default_values.sql
@@ -0,0 +1,11 @@
+-- Verify buildings:buildings/default_values on pg
+
+BEGIN;
+
+SELECT 1/count(*)
+FROM buildings.lifecycle_stage;
+
+SELECT 1/count(*)
+FROM buildings.use;
+
+ROLLBACK;
