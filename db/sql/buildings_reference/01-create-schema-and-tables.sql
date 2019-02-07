fd7e12b5fec257c3b784c8bb4f73538798ed76e6
Daniel Silk <dsilk@linz.govt.nz>
2019-02-07 13:28:39 +1300

    Reformat materialised view creation statement

--- db/sql/buildings_reference/01-create-schema-and-tables.sql
+++ db/sql/buildings_reference/01-create-schema-and-tables.sql
@@ -109,46 +109,52 @@ COMMENT ON COLUMN buildings_reference.territorial_authority.name IS
 
 CREATE MATERIALIZED VIEW buildings_reference.territorial_authority_grid AS
 -- Get extent of TA table and x and y grid number counts
-WITH nzext AS (
+WITH nz_extent AS (
     SELECT
-        ST_SetSRID(CAST(ST_Extent(shape) AS geometry),
-        2193) AS geom_ext, 300 AS x_gridcount, 250 AS y_gridcount
+          ST_SetSRID(CAST(ST_Extent(shape) AS geometry), 2193) AS geom_extent
+        , 300 AS x_grid_count
+        , 250 AS y_grid_count
     FROM buildings_reference.territorial_authority
 ),
 -- Get grid dimensions: x and y lengths and overall extent
 grid_dim AS (
     SELECT
-        (ST_XMax(geom_ext) - ST_XMin(geom_ext)) / x_gridcount AS g_width,
-        ST_XMin(geom_ext) AS xmin, ST_XMax(geom_ext) AS xmax,
-        (ST_YMax(geom_ext) - ST_YMin(geom_ext)) / y_gridcount AS g_height,
-        ST_YMin(geom_ext) AS ymin, ST_YMax(geom_ext) AS ymax
-    FROM nzext
+          (ST_XMax(geom_extent) - ST_XMin(geom_extent)) / x_grid_count AS g_width
+        , ST_XMin(geom_extent) AS xmin
+        , ST_XMax(geom_extent) AS xmax
+        , (ST_YMax(geom_extent) - ST_YMin(geom_extent)) / y_grid_count AS g_height
+        , ST_YMin(geom_extent) AS ymin
+        , ST_YMax(geom_extent) AS ymax
+    FROM nz_extent
 ),
 -- Divide TA extent into grid
 grid AS (
-    SELECT
-        ST_MakeEnvelope(
-            xmin + (x - 1) * g_width, ymin + (y - 1) * g_height,
-            xmin + x * g_width, ymin + y * g_height,
-            2193
+    SELECT ST_MakeEnvelope(
+          xmin + (x - 1) * g_width
+        , ymin + (y - 1) * g_height
+        , xmin + x * g_width
+        , ymin + y * g_height
+        , 2193
     ) AS grid_geom
-    FROM
-        (SELECT generate_series(1, x_gridcount) FROM nzext) AS x(x)
-        CROSS JOIN
-        (SELECT generate_series(1, y_gridcount) FROM nzext) AS y(y)
-        CROSS JOIN
-        grid_dim
+    FROM (
+        SELECT generate_series(1, x_grid_count)
+        FROM nz_extent
+    ) AS x(x)
+    CROSS JOIN (
+        SELECT generate_series(1, y_grid_count)
+        FROM nz_extent
+    ) AS y(y)
+    CROSS JOIN grid_dim
 )
 -- Select TA attributes and cut grid by TA boundaries
 SELECT
-    ROW_NUMBER() OVER(ORDER BY territorial_authority_id DESC) AS territorial_authority_grid_id,
-    territorial_authority_id AS territorial_authority_id,
-    external_territorial_authority_id AS external_territorial_authority_id,
-    name AS name,
-    st_intersection(ta.shape, grid_geom) AS shape
-
-FROM buildings_reference.territorial_authority AS ta
-JOIN grid AS g ON st_intersects(ta.shape, g.grid_geom);
+      row_number() OVER(ORDER BY territorial_authority_id DESC) AS territorial_authority_grid_id
+    , territorial_authority_id
+    , external_territorial_authority_id
+    , name
+    , ST_Intersection(ta.shape, g.grid_geom) AS shape
+FROM buildings_reference.territorial_authority ta
+JOIN grid g ON ST_Intersects(ta.shape, g.grid_geom);
 
 
 DROP INDEX IF EXISTS shx_territorial_authority_grid;
