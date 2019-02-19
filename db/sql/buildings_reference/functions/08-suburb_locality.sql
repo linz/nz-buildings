fd3f052ff8dd10b93c6519acaa5822cb75d28820
Megan Davidson <MDavidson@linz.govt.nz>
2019-02-20 11:29:41 +1300

    'insert' function

--- db/sql/buildings_reference/functions/08-suburb_locality.sql
+++ db/sql/buildings_reference/functions/08-suburb_locality.sql
@@ -15,6 +15,10 @@
     -- params:
     -- return: integer number of outlines deleted
 
+-- suburb_locality_insert_new_areas (insert new areas from admin_bdys)
+    -- params:
+    -- return: integer number of areas inserted
+
 --------------------------------------------
 
 -- Functions
@@ -93,3 +97,32 @@ LANGUAGE sql VOLATILE;
 
 COMMENT ON FUNCTION buildings_reference.suburb_locality_delete_removed_areas() IS
 'Function to delete from the buildings_reference suburb locality table the areas that have been removed in the admin_bdys schema';
+
+-- suburb_locality_insert_new_areas (insert new areas from admin_bdys)
+    -- params:
+    -- return: integer number of areas inserted
+CREATE OR REPLACE FUNCTION buildings_reference.suburb_locality_insert_new_areas()
+RETURNS integer AS
+$$
+    WITH insert_suburb AS (
+        INSERT INTO buildings_reference.suburb_locality (external_suburb_locality_id, suburb_4th, suburb_3rd, suburb_2nd, suburb_1st, shape)
+        SELECT
+          id,
+          suburb_4th,
+          suburb_3rd,
+          suburb_2nd,
+          suburb_1st,
+          ST_SetSRID(ST_Transform(shape, 2193), 2193)
+        FROM admin_bdys.nz_locality
+        WHERE id NOT IN (SELECT
+          external_suburb_locality_id
+        FROM buildings_reference.suburb_locality)
+        RETURNING *
+    )
+    SELECT count(*)::integer FROM insert_suburb;
+
+$$
+LANGUAGE sql VOLATILE;
+
+COMMENT ON FUNCTION buildings_reference.suburb_locality_insert_new_areas() IS
+'Function to insert from the admin_bdys schema new areas not in the buildings_reference suburb locality table';
