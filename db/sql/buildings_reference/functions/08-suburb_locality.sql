d57f8cff0b274fbc2b4375970b9329520193d0ca
Megan Davidson <MDavidson@linz.govt.nz>
2019-02-20 11:29:02 +1300

    'delete' function

--- db/sql/buildings_reference/functions/08-suburb_locality.sql
+++ db/sql/buildings_reference/functions/08-suburb_locality.sql
@@ -10,6 +10,11 @@
 -- bulk_load_outlines_update_suburb (replace suburb values with the intersection result)
     -- params: integer supplied_dataset_id
     -- return: count(integer) number of building outlines updated
+
+-- suburb_locality_delete_removed_areas (delete suburbs that are no longer is admin_bdys)
+    -- params:
+    -- return: integer number of outlines deleted
+
 --------------------------------------------
 
 -- Functions
@@ -65,3 +70,26 @@ LANGUAGE sql VOLATILE;
 
 COMMENT ON FUNCTION buildings_reference.bulk_load_outlines_update_suburb(integer) IS
 'Replace suburb values with the intersection result';
+
+-- update suburb_table_functions
+
+-- suburb_locality_delete_removed_areas (delete suburbs that are no longer is admin_bdys)
+    -- params:
+    -- return: integer number of outlines deleted
+CREATE OR REPLACE FUNCTION buildings_reference.suburb_locality_delete_removed_areas()
+RETURNS integer AS
+$$
+    WITH delete_suburb AS (
+        DELETE FROM buildings_reference.suburb_locality
+        WHERE external_suburb_locality_id NOT
+          IN (SELECT id
+          FROM admin_bdys.nz_locality)
+        RETURNING *
+    )
+    SELECT count(*)::integer FROM delete_suburb;
+
+$$
+LANGUAGE sql VOLATILE;
+
+COMMENT ON FUNCTION buildings_reference.suburb_locality_delete_removed_areas() IS
+'Function to delete from the buildings_reference suburb locality table the areas that have been removed in the admin_bdys schema';
