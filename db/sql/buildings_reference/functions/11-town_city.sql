c4894976ea05a7d7905638edcf8bb4a5ec2eca80
Megan Davidson <MDavidson@linz.govt.nz>
2019-02-20 14:39:27 +1300

    'insert' function

--- db/sql/buildings_reference/functions/11-town_city.sql
+++ db/sql/buildings_reference/functions/11-town_city.sql
@@ -15,6 +15,10 @@
     -- params:
     -- return: integer number of town_cities deleted
 
+-- town_city_insert_new_areas (insert new areas from admin_bdys)
+    -- params:
+    -- return: integer number of outlines inserted
+
 --------------------------------------------
 
 -- Functions:
@@ -92,3 +96,36 @@ LANGUAGE sql VOLATILE;
 
 COMMENT ON FUNCTION buildings_reference.town_city_delete_removed_areas() IS
 'Function to delete from the buildings_reference town city table the areas that have been removed in the admin_bdys schema';
+
+-- town_city_insert_new_areas (insert new areas from admin_bdys)
+    -- params:
+    -- return: integer number of outlines inserted
+CREATE OR REPLACE FUNCTION buildings_reference.town_city_insert_new_areas()
+RETURNS integer AS
+$$
+    WITH insert_town AS (
+        INSERT INTO buildings_reference.town_city (external_city_id, name, shape)
+          SELECT
+            subquery.city_id,
+            subquery.city_name,
+            ST_SetSRID(ST_Transform(subquery.shape, 2193), 2193)
+          FROM (SELECT
+            city_id,
+            city_name,
+            ST_Multi(ST_Union(nzl.shape)) AS shape
+          FROM admin_bdys.nz_locality AS nzl
+          WHERE city_id != 0
+          AND city_id NOT IN (SELECT
+            external_city_id
+          FROM buildings_reference.town_city)
+          GROUP BY city_id,
+                   city_name) AS subquery
+        RETURNING *
+    )
+    SELECT count(*)::integer FROM insert_town;
+
+$$
+LANGUAGE sql VOLATILE;
+
+COMMENT ON FUNCTION buildings_reference.town_city_insert_new_areas() IS
+'Function to insert from the admin_bdys schema new areas not in the buildings_reference town city table';
