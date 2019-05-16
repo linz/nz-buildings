86b84f3880608cb5c464685769f69691ca817e8f
YIngting Chen <YChen@linz.govt.nz>
2019-05-17 11:24:02 +1200

    Set up new toolbar

--- buildings/gui/production_frame.py
+++ buildings/gui/production_frame.py
@@ -5,7 +5,7 @@ import math
 import os.path
 
 from PyQt4 import uic
-from PyQt4.QtCore import pyqtSlot, Qt
+from PyQt4.QtCore import pyqtSlot, Qt, QSize
 from PyQt4.QtGui import QAction, QColor, QFrame, QIcon, QMenu
 from qgis.core import QgsFeature, QgsGeometry, QgsPoint, QgsProject, QgsVectorLayer, QgsMapLayerRegistry
 from qgis.gui import QgsMessageBar, QgsRubberBand
@@ -18,7 +18,7 @@ from buildings.utilities import layers
 from buildings.utilities.layers import LayerRegistry
 from buildings.utilities.point_tool import PointTool
 
-
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'production_edits.ui'))
 
@@ -39,45 +39,50 @@ class ProductionFrame(QFrame, FORM_CLASS):
         self.add_outlines()
         # Set up edit dialog
         self.edit_dialog = EditDialog(self)
-        # editing fields
-        self.added_building_ids = []
-        self.geom = None
-        self.ids = []
-        self.geoms = {}
-        self.building_outline_id = None
-        self.edit_status = None
         self.change_instance = None
 
         self.cb_production.setChecked(True)
 
-        self.menu = QMenu()
-        self.action_add_outline = QAction('Add Outline', self.menu)
-        self.action_edit_attribute = QAction('Edit Attribute', self.menu)
-        self.action_edit_geometry = QAction('Edit Geometry', self.menu)
-        self.menu.addAction(self.action_add_outline)
-        self.menu.addSeparator()
-        self.menu.addAction(self.action_edit_attribute)
-        self.menu.addAction(self.action_edit_geometry)
-        self.menu.setFixedWidth(300)
-        self.tbtn_edits.setMenu(self.menu)
-        self.layout_capture_method.hide()
-        self.layout_general_info.hide()
-        self.layout_end_lifespan.hide()
-        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        self.btn_circle.setIcon(QIcon(os.path.join(__location__, '..', 'icons', 'circle.png')))
-
         # set up signals and slots
-        self.tbtn_edits.triggered.connect(self.tbtn_edits_triggered)
-        self.tbtn_edits.clicked.connect(self.tbtn_edits_clicked)
         self.btn_exit.clicked.connect(self.exit_clicked)
-        self.btn_exit_edits.clicked.connect(self.edit_cancel_clicked)
         self.cb_production.clicked.connect(self.cb_production_clicked)
         QgsMapLayerRegistry.instance().layerWillBeRemoved.connect(self.layers_removed)
 
-        # self.btn_edit_save.setDisabled(1)
-        # self.btn_edit_reset.setDisabled(1)
-        # self.btn_exit_edits.setDisabled(1)
-        self.btn_circle.hide()
+        self.setup_toolbar()
+
+    def setup_toolbar(self):
+
+        if 'Add Outline' not in (action.text() for action in iface.building_toolbar.actions()):
+            image_dir = os.path.join(__location__, '..', 'icons')
+            icon_path = os.path.join(image_dir, "plus.png")
+            icon = QIcon()
+            icon.addFile(icon_path, QSize(8, 8))
+            self.add_action = QAction(icon, "Add Outline", iface.building_toolbar)
+            iface.registerMainWindowAction(self.add_action, "Ctrl+1")
+            self.add_action.triggered.connect(self.canvas_add_outline)
+            iface.building_toolbar.addAction(self.add_action)
+
+        if 'Edit Geometry' not in (action.text() for action in iface.building_toolbar.actions()):
+            image_dir = os.path.join(__location__, '..', 'icons')
+            icon_path = os.path.join(image_dir, "edit_geometry.png")
+            icon = QIcon()
+            icon.addFile(icon_path, QSize(8, 8))
+            self.edit_geom_action = QAction(icon, "Edit Geometry", iface.building_toolbar)
+            iface.registerMainWindowAction(self.edit_geom_action, "Ctrl+2")
+            self.edit_geom_action.triggered.connect(self.canvas_edit_geometry)
+            iface.building_toolbar.addAction(self.edit_geom_action)
+
+        if 'Edit Attributes' not in (action.text() for action in iface.building_toolbar.actions()):
+            image_dir = os.path.join(__location__, '..', 'icons')
+            icon_path = os.path.join(image_dir, "edit_attributes.png")
+            icon = QIcon()
+            icon.addFile(icon_path, QSize(8, 8))
+            self.edit_attrs_action = QAction(icon, "Edit Attributes", iface.building_toolbar)
+            iface.registerMainWindowAction(self.edit_attrs_action, "Ctrl+3")
+            self.edit_attrs_action.triggered.connect(self.canvas_edit_attribute)
+            iface.building_toolbar.addAction(self.edit_attrs_action)
+
+        iface.building_toolbar.show()
 
     def add_outlines(self):
         """
@@ -107,39 +112,6 @@ class ProductionFrame(QFrame, FORM_CLASS):
         else:
             group.setVisible(Qt.Unchecked)
 
-    @pyqtSlot(QAction)
-    def tbtn_edits_triggered(self, action):
-        """
-            When edit tool button triggered
-        """
-        self.tbtn_edits.setDefaultAction(action)
-        text = action.text()
-        self.enter_edit_mode(text)
-
-    @pyqtSlot()
-    def tbtn_edits_clicked(self):
-        """
-            When edit tool button clicked
-        """
-        text = self.tbtn_edits.text()
-        if text != 'Choose Edit Mode':
-            self.enter_edit_mode(text)
-        else:
-            self.tbtn_edits.showMenu()
-
-    def enter_edit_mode(self, text):
-
-        # self.btn_edit_reset.setEnabled(True)
-        # self.btn_exit_edits.setEnabled(True)
-        self.tbtn_edits.setEnabled(False)
-        self.tbtn_edits.setStyleSheet('QToolButton {color: green;}')
-        if text == 'Add Outline':
-            self.canvas_add_outline()
-        elif text == 'Edit Attribute':
-            self.canvas_edit_attribute()
-        elif text == 'Edit Geometry':
-            self.canvas_edit_geometry()
-
     def canvas_add_outline(self):
         """
             When add outline radio button toggled
@@ -148,49 +120,17 @@ class ProductionFrame(QFrame, FORM_CLASS):
         self.edit_dialog.add_outline()
         self.change_instance = self.edit_dialog.get_change_instance()
 
-        # self.geom = None
-        self.polyline = None
         self.circle_tool = None
-        # self.added_building_ids = []
-        # iface.actionCancelEdits().trigger()
-        # # reset toolbar
-        # for action in iface.building_toolbar.actions():
-        #     if action.objectName() not in ['mActionPan']:
-        #         iface.building_toolbar.removeAction(action)
-        # # set change instance to added class
-        # try:
-        #     self.btn_edit_save.clicked.disconnect()
-        # except TypeError:
-        #     pass
-        # try:
-        #     self.btn_edit_reset.clicked.disconnect()
-        # except TypeError:
-        #     pass
-        # try:
-        #     self.btn_exit_edits.clicked.disconnect()
-        # except Exception:
-        #     pass
-        # self.layout_capture_method.show()
-        # self.layout_general_info.show()
-        # self.layout_end_lifespan.show()
-        # self.change_instance = production_changes.AddProduction(self)
-        # set up circle button
-        self.btn_circle.clicked.connect(self.setup_circle)
-        self.btn_circle.show()
-        # # connect signals and slots
-        # self.btn_edit_save.clicked.connect(partial(self.change_instance.save_clicked, True))
-        # self.btn_edit_reset.clicked.connect(self.change_instance.reset_clicked)
-        # self.btn_exit_edits.clicked.connect(self.edit_cancel_clicked)
-        # self.building_layer.featureAdded.connect(self.change_instance.creator_feature_added)
-        # self.building_layer.featureDeleted.connect(self.change_instance.creator_feature_deleted)
-        # self.building_layer.geometryChanged.connect(self.change_instance.creator_geometry_changed)
-        # # add territorial Authority layer
-        # self.territorial_auth = self.layer_registry.add_postgres_layer(
-        #     'territorial_authorities', 'territorial_authority',
-        #     'shape', 'buildings_reference', '', ''
-        # )
-        # layers.style_layer(self.territorial_auth,
-        #                    {1: ['204,121,95', '0.3', 'dash', '5;2']})
+        self.polyline = None
+        image_dir = os.path.join(__location__, '..', 'icons')
+        icon_path = os.path.join(image_dir, "circle.png")
+        icon = QIcon()
+        icon.addFile(icon_path, QSize(8, 8))
+        self.circle_action = QAction(icon, "Draw Circle", iface.building_toolbar)
+        iface.registerMainWindowAction(self.circle_action, "Ctrl+0")
+        self.circle_action.triggered.connect(self.setup_circle)
+        self.circle_action.setCheckable(True)
+        iface.building_toolbar.addAction(self.circle_action)
 
     def canvas_edit_attribute(self):
         """
@@ -199,89 +139,17 @@ class ProductionFrame(QFrame, FORM_CLASS):
         self.edit_dialog.show()
         self.edit_dialog.edit_attribute()
         self.change_instance = self.edit_dialog.get_change_instance()
-        # iface.actionCancelEdits().trigger()
-        # # reset toolbar
-        # for action in iface.building_toolbar.actions():
-        #     if action.objectName() not in ['mActionPan']:
-        #         iface.building_toolbar.removeAction(action)
-        # # set change instance to edit class
-        # try:
-        #     self.btn_edit_save.clicked.disconnect()
-        # except Exception:
-        #     pass
-        # try:
-        #     self.btn_edit_reset.clicked.disconnect()
-        # except Exception:
-        #     pass
-        # try:
-        #     self.btn_exit_edits.clicked.disconnect()
-        # except Exception:
-        #     pass
-        # try:
-        #     self.btn_end_lifespan.clicked.disconnect()
-        # except Exception:
-        #     pass
-        # self.layout_capture_method.show()
-        # self.layout_general_info.show()
-        # self.layout_end_lifespan.show()
-        # self.change_instance = production_changes.EditAttribute(self)
-        # # set up signals and slots
-        # self.btn_edit_save.clicked.connect(partial(self.change_instance.save_clicked, True))
-        # self.btn_edit_reset.clicked.connect(self.change_instance.reset_clicked)
-        # self.btn_exit_edits.clicked.connect(self.edit_cancel_clicked)
-        # self.btn_end_lifespan.clicked.connect(partial(self.change_instance.end_lifespan, True))
-        # self.building_layer.selectionChanged.connect(self.change_instance.selection_changed)
-        # # add territorial authority layer
-        # self.territorial_auth = self.layer_registry.add_postgres_layer(
-        #     'territorial_authorities', 'territorial_authority',
-        #     'shape', 'buildings_reference', '', ''
-        # )
-        # layers.style_layer(self.territorial_auth,
-        #                    {1: ['204,121,95', '0.3', 'dash', '5;2']})
 
     def canvas_edit_geometry(self):
         self.edit_dialog.edit_geometry()
         self.edit_dialog.show()
         self.change_instance = self.edit_dialog.get_change_instance()
 
-        # iface.actionCancelEdits().trigger()
-        # # reset toolbar
-        # for action in iface.building_toolbar.actions():
-        #     if action.objectName() not in ['mActionPan']:
-        #         iface.building_toolbar.removeAction(action)
-        # # set change instance to edit class
-        # try:
-        #     self.btn_edit_save.clicked.disconnect()
-        # except Exception:
-        #     pass
-        # try:
-        #     self.btn_edit_reset.clicked.disconnect()
-        # except Exception:
-        #     pass
-        # try:
-        #     self.btn_exit_edits.clicked.disconnect()
-        # except Exception:
-        #     pass
-        # self.layout_capture_method.show()
-        # self.layout_general_info.hide()
-        # self.layout_end_lifespan.hide()
-        # self.change_instance = production_changes.EditGeometry(self)
-        # # set up signals and slots
-        # self.btn_edit_save.clicked.connect(partial(self.change_instance.save_clicked, True))
-        # self.btn_edit_reset.clicked.connect(self.change_instance.reset_clicked)
-        # self.btn_exit_edits.clicked.connect(self.edit_cancel_clicked)
-        # self.building_layer.geometryChanged.connect(self.change_instance.geometry_changed)
-        # # add territorial authority layer
-        # self.territorial_auth = self.layer_registry.add_postgres_layer(
-        #     'territorial_authorities', 'territorial_authority',
-        #     'shape', 'buildings_reference', '', ''
-        # )
-        # layers.style_layer(self.territorial_auth,
-        #                    {1: ['204,121,95', '0.3', 'dash', '5;2']})
-
     @pyqtSlot()
     def setup_circle(self):
-        # called when draw circle button is clicked
+        """
+            called when draw circle button is clicked
+        """
         self.points = []
         # set map tool to new point tool
         self.circle_tool = PointTool(iface.mapCanvas())
@@ -297,7 +165,9 @@ class ProductionFrame(QFrame, FORM_CLASS):
 
     @pyqtSlot(QgsPoint)
     def draw_circle(self, point):
-        # called when mapcanvas is clicked
+        """
+            called when mapcanvas is clicked
+        """
         self.points.append(point)
         self.polyline.addPoint(point, True)
         self.polyline.setToGeometry(QgsGeometry.fromPolyline(self.points), None)
@@ -311,17 +181,19 @@ class ProductionFrame(QFrame, FORM_CLASS):
             point = QgsGeometry.fromPoint(QgsPoint(self.points[0]))
             # create buffer of specified distance around point
             buffer = point.buffer(radius, nodes)
-            # add feature to building_outlines (triggering featureAdded)
-            self.feature = QgsFeature(self.building_layer.pendingFields())
+            # add feature to bulk_load_outlines (triggering featureAdded)
+            self.feature = QgsFeature(self.bulk_load_layer.pendingFields())
             self.feature.setGeometry(buffer)
-            self.building_layer.addFeature(self.feature)
-            self.building_layer.triggerRepaint()
+            self.bulk_load_layer.addFeature(self.feature)
+            self.bulk_load_layer.triggerRepaint()
             # reset points list
             self.points = []
 
     @pyqtSlot(QgsPoint)
     def update_line(self, point):
-        # called when mouse moved on canvas
+        """
+            called when mouse moved on canvas
+        """
         if len(self.points) == 1:
             # if the center has been clicked have a line follow the mouse movement
             line = [self.points[0], point]
@@ -332,7 +204,6 @@ class ProductionFrame(QFrame, FORM_CLASS):
         """
         Called when edit production exit button clicked.
         """
-        self.edit_cancel_clicked()
         self.close_frame()
         self.dockwidget.lst_sub_menu.clearSelection()
 
@@ -340,7 +211,8 @@ class ProductionFrame(QFrame, FORM_CLASS):
         """
         Clean up and remove the edit production frame.
         """
-        # reload layers
+        if self.change_instance is not None:
+            self.edit_dialog.close()
         iface.actionCancelEdits().trigger()
         QgsMapLayerRegistry.instance().layerWillBeRemoved.disconnect(self.layers_removed)
         self.layer_registry.remove_layer(self.building_layer)
@@ -393,29 +265,20 @@ class ProductionFrame(QFrame, FORM_CLASS):
                     self.circle_tool.deactivate()
                 iface.actionPan().trigger()
 
-        self.tbtn_edits.setEnabled(True)
-        self.tbtn_edits.setStyleSheet('QToolButton {color: black;}')
-
         iface.actionCancelEdits().trigger()
         # reload layers
         QgsMapLayerRegistry.instance().layerWillBeRemoved.disconnect(self.layers_removed)
         self.edit_dialog.remove_territorial_auth()
         QgsMapLayerRegistry.instance().layerWillBeRemoved.connect(self.layers_removed)
 
-        self.btn_circle.hide()
-
+        self.setup_toolbar()
         self.change_instance = None
 
     @pyqtSlot(str)
     def layers_removed(self, layerids):
         self.layer_registry.update_layers()
-        layers = ['building_outlines', 'historic_outlines', 'territorial_authorities']
-        for layer in layers:
+        for layer in ['building_outlines', 'historic_outlines', 'territorial_authorities']:
             if layer in layerids:
-                # self.btn_edit_save.setDisabled(1)
-                # self.btn_edit_reset.setDisabled(1)
-                # self.btn_exit_edits.setDisabled(1)
-                self.tbtn_edits.setDisabled(1)
                 self.cb_production.setDisabled(1)
                 iface.messageBar().pushMessage("ERROR",
                                                "Required layer Removed! Please reload the buildings plugin or the current frame before continuing",
