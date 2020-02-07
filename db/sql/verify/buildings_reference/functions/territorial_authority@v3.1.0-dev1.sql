b0401fc1543518ea355e3c25bb3a5982c1e96873
Daniel Silk <dsilk@linz.govt.nz>
2020-02-07 14:29:18 +1300

    fix: Territorial Authority reference data update join correction

--- db/sql/verify/buildings_reference/functions/territorial_authority@v3.1.0-dev1.sql
+++ db/sql/verify/buildings_reference/functions/territorial_authority@v3.1.0-dev1.sql
@@ -12,17 +12,4 @@ SELECT has_function_privilege('buildings_reference.territorial_auth_insert_areas
 
 SELECT has_function_privilege('buildings_reference.territorial_auth_update_areas()', 'execute');
 
-DO $$
-BEGIN
-
-    PERFORM proname, proargnames, prosrc 
-    FROM pg_proc
-    WHERE proname = 'territorial_auth_update_areas'
-    AND prosrc LIKE '%JOIN%';
-    IF FOUND THEN
-        RAISE EXCEPTION 'JOIN found.';
-    END IF;
-
-END $$;
-
 ROLLBACK;
