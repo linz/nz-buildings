484fa96627267945b6eeaef7fb7082c70c99d880
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2022-03-21 07:28:15 +1300

    fix: added buildings within facilities

--- buildings/gui/comparisons.py
+++ buildings/gui/comparisons.py
@@ -81,5 +81,15 @@ def compare_outlines(self, commit_status):
         sql = "UPDATE buildings_bulk_load.bulk_load_outlines blo set bulk_load_use_id = use_id FROM (SELECT use_id, building_id, bulk_load_outline_id, building_outline_id FROM buildings.building_use bu JOIN (SELECT building_outline_id, building_id, bulk_load_outline_id FROM buildings.building_outlines JOIN (SELECT bulk_load_outline_id, building_outline_id, qa_status_id FROM buildings_bulk_load.related JOIN buildings_bulk_load.related_bulk_load_outlines USING (bulk_load_outline_id) WHERE qa_status_id = 1) s1 USING (building_outline_id)) s3 USING (building_id)) s4 WHERE blo.bulk_load_outline_id = s4.bulk_load_outline_id;"
         self.db.execute_no_commit(sql)
 
+        # populate buildings_bulk_load.bulk_load_outlines with bulk_load_name if an added building exists more than 50% inside a facility polygon.
+        # This assumes you have a facilities_lds schema in your db.
+        sql = "UPDATE buildings_bulk_load.bulk_load_outlines blo set bulk_load_name = name FROM (SELECT bulk_load_outline_id, name FROM buildings_bulk_load.bulk_load_outlines blo JOIN ((WITH added_facility_intersects AS ( SELECT bulk_load_outline_id, fac.name, ST_Area(ST_Intersection(supplied.shape, fac.shape)) / ST_Area(supplied.shape) * 100 AS 'supplied_intersect' FROM facilities_lds.nz_facilities fac JOIN (SELECT bulk_load_outline_id, qa_status_id, shape FROM buildings_bulk_load.added supplied JOIN buildings_bulk_load.added_outlines addedshapes USING (bulk_load_outline_id) WHERE qa_status_id = 1) supplied ON ST_Intersects(supplied.shape, fac.shape)) SELECT * FROM added_facility_intersects WHERE 'supplied_intersect' > 50)) s1 USING (bulk_load_outline_id)) added WHERE added.bulk_load_outline_id = blo.bulk_load_outline_id;"
+        self.db.execute_no_commit(sql)
+
+        # populate buildings_bulk_load.bulk_load_outlines with bulk_load_use_id if an added building exists more than 50% inside a facility polygon.
+        # This assumes you have a facilities_lds schema in your db.
+        sql = "UPDATE buildings_bulk_load.bulk_load_outlines blo set bulk_load_use_id = use_id FROM (SELECT bulk_load_outline_id, CASE use WHEN 'hospital' THEN 16 WHEN 'school' THEN 27 END use_id FROM (SELECT bulk_load_outline_id, use FROM buildings_bulk_load.bulk_load_outlines blo JOIN ((WITH added_facility_intersects AS ( SELECT bulk_load_outline_id, lower(fac.use) AS use, ST_Area(ST_Intersection(supplied.shape, fac.shape)) / ST_Area(supplied.shape) * 100 AS 'supplied_intersect' FROM facilities_lds.nz_facilities fac JOIN (SELECT bulk_load_outline_id, qa_status_id, shape FROM buildings_bulk_load.added supplied JOIN buildings_bulk_load.added_outlines addedshapes USING (bulk_load_outline_id) WHERE qa_status_id = 1) supplied ON ST_Intersects(supplied.shape, fac.shape)) SELECT * FROM added_facility_intersects WHERE 'supplied_intersect' > 50)) s1 USING (bulk_load_outline_id)) use_select) added WHERE added.bulk_load_outline_id = blo.bulk_load_outline_id;"
+        self.db.execute_no_commit(sql)
+
     if commit_status:
         self.db.commit_open_cursor()
