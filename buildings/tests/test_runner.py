dd3af69a4d38eaf6e3c89d464012a2f1dc84a6a8
Andrew Jacombs <ajacombs@linz.govt.nz>
2022-08-01 16:54:20 +1200

    ci: Log more detailed error info

--- buildings/tests/test_runner.py
+++ buildings/tests/test_runner.py
@@ -11,4 +11,4 @@ def run_test_modules():
     unique test modules
     """
     test_suite = unittest.TestLoader().discover(__location__, pattern="test_*.py")
-    unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(test_suite)
+    unittest.TextTestRunner(verbosity=3, stream=sys.stderr, tb_locals=True, descriptions=True).run(test_suite)
