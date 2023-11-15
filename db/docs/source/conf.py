2331c650593f209b3ccb4a04538e9a44190e3b8a
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2023-11-15 14:05:06 +1300

    docs: remove rtd theme

--- db/docs/source/conf.py
+++ db/docs/source/conf.py
@@ -37,7 +37,7 @@ sys.path.insert(0, os.path.abspath("../../sql/deploy"))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # extensions = ["sphinx.ext.autodoc"]
-extensions = ["sphinx_rtd_theme"]
+# extensions = ["sphinx_rtd_theme"]
 
 # 'sphinx.ext.autosectionlabel'
 # Add any paths that contain templates here, relative to this directory.
@@ -117,7 +117,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
