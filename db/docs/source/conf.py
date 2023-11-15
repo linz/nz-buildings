e1f14980c0b194d9f336b529682ec3fddb29b3e2
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2023-11-16 10:38:48 +1300

    docs: update logo, switch text

--- db/docs/source/conf.py
+++ db/docs/source/conf.py
@@ -137,7 +137,7 @@ html_short_title = "NZ Buildings Data Dictionary"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/logo.png"
+html_logo = "_static/logo2.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
