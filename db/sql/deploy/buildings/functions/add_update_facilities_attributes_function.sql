fcab95cdb539d3e62a43e0cadd0eba1513fb56fd
Douglas Kwan <dkwan@linz.govt.nz>
2025-11-17 12:41:44 +1300

    Chore: Descriptive comments

--- db/sql/deploy/buildings/functions/add_update_facilities_attributes_function.sql
+++ db/sql/deploy/buildings/functions/add_update_facilities_attributes_function.sql
@@ -17,13 +17,16 @@ BEGIN;
 
 -- TODO - Add separate check before this step to check all facility USEs are
 --        matching buildings.use values corresponding to USE_IDs.
---      - Add comments
 
 
+------------------------------------------------------------------------------
 -- BUILDING NAME MODIFY
+-- Where building NAME does not match facility polygon NAME, modify building NAME.
+------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION buildings.update_facilities_name_modify()
 RETURNS integer AS
 $$
+    -- Select buildings and names intersecting facility polygons, excluding retired buildings
     WITH bo_intersects_fac AS (
         SELECT 
             building_id AS bo_building_id,
@@ -45,6 +48,10 @@ $$
 			WHERE bo.end_lifespan is NULL
         ) bo ON ST_Intersects(bo.shape, fac.shape)
     ),
+    -- Filter buildings:
+    --   - More than half the geometry within facility polygon
+    --   - Has a different name to facility name
+    --   - Has a current name
     bo_in_fac AS (
         SELECT *
         FROM bo_intersects_fac
@@ -52,12 +59,14 @@ $$
             AND building_name != fac_name
             AND bn_end_lifespan IS NULL
     ),
+    -- Retire old building name
     updated AS (
         UPDATE buildings.building_name b_name
         SET end_lifespan = NOW()
         FROM bo_in_fac
         WHERE b_name.building_id = bo_in_fac.bo_building_id
     ),
+    -- Add new building name 
     inserted AS (
 		INSERT INTO buildings.building_name (building_name_id, building_id, building_name, begin_lifespan)
 	    SELECT nextval('buildings.building_name_building_name_id_seq'),
@@ -66,15 +75,20 @@ $$
 	        NOW()
 	    FROM bo_in_fac
 	)
+    -- Return count for reporting 
     SELECT count(*)::integer FROM bo_in_fac;
 $$
 LANGUAGE sql VOLATILE;
 
 
+------------------------------------------------------------------------------
 -- BUILDING USE MODIFY
+-- Where building USE does not match facility polygon USE, modify building USE.
+------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION buildings.update_facilities_use_modify()
 RETURNS integer AS
 $$
+    -- Select buildings and uses intersecting facility polygons, excluding retired buildings
     WITH bo_intersects_fac AS (
         SELECT 
             building_id AS bo_building_id,
@@ -97,6 +111,10 @@ $$
 			WHERE bo.end_lifespan is NULL
         ) bo ON ST_Intersects(bo.shape, fac.shape)
     ),
+    -- Filter buildings:
+    --   - More than half the geometry within facility polygon
+    --   - Has a different use to facility use
+    --   - Has a current use
     bo_in_fac AS (
         SELECT *
         FROM bo_intersects_fac
@@ -104,12 +122,14 @@ $$
             AND use_value != fac_use
             AND bu_end_lifespan IS NULL
     ),
+    -- Retire old building use
     updated AS (
         UPDATE buildings.building_use b_use
         SET end_lifespan = NOW()
         FROM bo_in_fac
         WHERE b_use.building_id = bo_in_fac.bo_building_id
     ),
+    -- Add new building use 
     inserted AS (
 		INSERT INTO buildings.building_use (building_use_id, building_id, use_id, begin_lifespan)
 	    SELECT nextval('buildings.building_use_building_use_id_seq'),
@@ -118,15 +138,20 @@ $$
 	        NOW()
 	    FROM bo_in_fac
 	)
+    -- Return count for reporting
     SELECT count(*)::integer FROM bo_in_fac;
 $$
 LANGUAGE sql VOLATILE;
 
 
+------------------------------------------------------------------------------
 -- BUILDING NAME ADD
+-- Where building has no NAME and facility polygon has NAME, add building NAME.
+------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION buildings.update_facilities_name_add()
 RETURNS integer AS
 $$
+    -- Select buildings intersecting facility polygons, excluding retired buildings
     WITH bo_in_fac AS (
         WITH bo_intersects_fac AS (
             SELECT 
@@ -142,10 +167,12 @@ $$
                 WHERE bo.end_lifespan is NULL
             ) bo ON ST_Intersects(bo.shape, fac.shape)
         )
+        -- Filter buildings to those with more than half the geometry within facility polygon
         SELECT *
         FROM bo_intersects_fac
         WHERE bo_intersect_ratio > 0.5
     ),
+    -- Join building name table using LEFT JOIN because building name will be empty
     building_name_joined AS (
     SELECT
         bo_in_fac.fac_name,
@@ -156,6 +183,7 @@ $$
     LEFT JOIN buildings.building_name bn USING (building_id)
     WHERE bn.building_name is NULL
     ),
+    -- Add new building name 
     inserted AS (
         INSERT INTO buildings.building_name (building_name_id, building_id, building_name, begin_lifespan)
         SELECT nextval('buildings.building_name_building_name_id_seq'),
@@ -164,15 +192,20 @@ $$
             NOW()
         FROM building_name_joined
     )
+    -- Return count for reporting 
     SELECT count(*)::integer FROM building_name_joined;
 $$
 LANGUAGE sql VOLATILE;
 
 
+------------------------------------------------------------------------------
 -- BUILDING USE ADD
+-- Where building has no USE and facility polygon has USE, add building USE.
+------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION buildings.update_facilities_use_add()
 RETURNS integer AS
 $$
+    -- Select buildings intersecting facility polygons, excluding retired buildings
     WITH bo_in_fac AS (
         WITH bo_intersects_fac AS (
             SELECT 
@@ -188,10 +221,12 @@ $$
                 WHERE bo.end_lifespan is NULL
             ) bo ON ST_Intersects(bo.shape, fac.shape)
         )
+        -- Filter buildings to those with more than half the geometry within facility polygon
         SELECT *
         FROM bo_intersects_fac
         WHERE bo_intersect_ratio > 0.5
     ),
+    -- Join building use table using LEFT JOIN because building use will be empty
     building_use_joined AS (
     SELECT
         bo_in_fac.fac_use,
@@ -203,6 +238,7 @@ $$
     LEFT JOIN buildings.use u USING (use_id)
     WHERE bu.building_use_id is NULL
     ),
+    -- Add new building use 
     inserted AS (
         INSERT INTO buildings.building_use (building_use_id, building_id, use_id, begin_lifespan)
         SELECT nextval('buildings.building_use_building_use_id_seq'),
@@ -211,15 +247,24 @@ $$
             NOW()
         FROM building_use_joined
     )
+    -- Return count for reporting 
     SELECT count(*)::integer FROM building_use_joined;
 $$
 LANGUAGE sql VOLATILE;
 
 
+------------------------------------------------------------------------------
 -- BUILDING NAME/USE REMOVE
+-- Where building has NAME/USE with no matching facility polygon, remove NAME/USE.
+-- Only remove NAME/USE for USE types included in NZ Facilities, currently
+-- these are Hospitals and Schools.  Other USE types do not have corresponding
+-- facility polygons to compare, so removal of NAME and USE needs to be in the same
+-- function.
+------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION buildings.update_facilities_name_use_remove()
 RETURNS integer AS
 $$
+    -- Select buildings with a use that is included in NZ Facilities, excluding retired uses and retired buildings
     WITH bo_has_use AS (
         SELECT
             building_id,
@@ -232,10 +277,11 @@ $$
         JOIN buildings.building_name bn USING (building_id)
         JOIN buildings.building_use bu USING (building_id)
         JOIN buildings.use u USING (use_id)
-        WHERE u.use_id IN (16,27) -- Hospital = 16, School = 27
+        WHERE u.use_id IN (16,27) -- use_id 16 = Hospital, 27 = School
             AND bu.end_lifespan IS NULL
             AND bo.end_lifespan IS NULL
     ),
+    -- Join facility polygons using intersects and LEFT JOIN because we want to include those with no intersect
     bo_outside_fac AS (
     SELECT 
         building_id,
@@ -253,9 +299,11 @@ $$
             bu_building_use_id,
             u_value,
             bo.shape
+    -- Filter buildings to those that don't intersect, or are less than half within, facility polygons
     HAVING 
         COALESCE(SUM(ST_Area(ST_Intersection(bo.shape, f.shape))) / NULLIF(ST_Area(bo.shape), 0), 0) < 0.5
     ),
+    -- Remove building name
     remove_name AS (
         UPDATE buildings.building_name b_name
         SET end_lifespan = NOW()
@@ -263,6 +311,7 @@ $$
         WHERE b_name.building_id = bo_outside_fac.building_id
             AND b_name.end_lifespan is NULL
     ),
+    -- Remove building use
     remove_use AS (
         UPDATE buildings.building_use b_use
         SET end_lifespan = NOW()
@@ -270,12 +319,17 @@ $$
         WHERE b_use.building_id = bo_outside_fac.building_id
             AND b_use.end_lifespan is NULL
     )
+    -- Return count for reporting 
     SELECT count(*)::integer FROM bo_outside_fac;
 $$
 LANGUAGE sql VOLATILE;
 
 
+------------------------------------------------------------------------------
 -- UPDATE FACILITIES ATTRIBUTES
+-- Wrapper to run all update facilities functions.
+-- Returns a table with each update type and the count of buildings updated.
+------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION buildings.update_facilities_attributes()
 RETURNS TABLE(update_type text, count integer) AS
 $$
